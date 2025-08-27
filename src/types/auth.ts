edf96b53ae77a0b40ca0b44bf71c589faf8e0c22
hao <lucas60303@gmail.com>
2025-08-27 11:14:58 +0800

    feat: add type of register account form data

---
+++ src/types/auth.ts
@@ -0,0 +1,11 @@
+export interface LoginFormData {
+  id: string
+  name: string
+  password: string
+  confirmPassword: string
+  primary_email: string
+  secondary_email: string
+  phone_number: string
+  position: string
+  rememberMe: boolean
+}
