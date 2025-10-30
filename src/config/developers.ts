d4241f3c651988fb983a54793e092212b6fdedcf
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-10-31 03:25:50 +0800

    feat: create developers config

---
+++ src/config/developers.ts
@@ -0,0 +1,123 @@
+export interface DeveloperConfig {
+  name: string
+  description?: string
+  avatar?: URL,
+  github?: string
+}
+
+export const developerConfig: DeveloperConfig[] = [
+  {
+    name: '張育綸',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316042.png', import.meta.url),
+    github: 'BlueBoy247',
+  },
+  {
+    name: '陳皓平',
+    description: '前端開發者，負責使用者介面與互動設計。前端開發者，負責使用者介面與互動設計。前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11216028.png', import.meta.url),
+    github: 'lucas6028',
+  },
+  {
+    name: '陳佳筠',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316010.png', import.meta.url),
+    github: 'Doreen-123',
+  },
+  {
+    name: '任晏磊',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316011.png', import.meta.url),
+    github: 'yanyu18927',
+  },
+  {
+    name: '鍾沅謀',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316015.png', import.meta.url),
+    github: 'morris950318',
+  },
+  {
+    name: '楊顓睿',
+    description: '前端開發者，負責使用者介面與互動設計。前端開發者，負責使用者介面與互動設計。前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316016.png', import.meta.url),
+    github: 'Roy-1011',
+  },
+  {
+    name: '陳胤華',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316017.png', import.meta.url),
+    github: 'Fabricator0417',
+  },
+  {
+    name: '洪瑋甯',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316022.png', import.meta.url),
+    github: 'ning-hong',
+  },
+  {
+    name: '謝秉錡',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316033.png', import.meta.url),
+    github: 'benjamin666520',
+  },
+  {
+    name: '許建程',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316035.png', import.meta.url),
+    github: 'jimkro',
+  },
+  {
+    name: '謝妤涵',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316043.png', import.meta.url),
+    github: '4hshany',
+  },
+  {
+    name: '王毓如',
+    description: '前端開發者，負責使用者介面與互動設計。',
+    avatar: new URL('/src/assets/avatars/U11316044.png', import.meta.url),
+    github: 'zowang2024',
+  },
+  {
+    name: '黃瑞麟',
+    description: '',
+    avatar: new URL('/src/assets/avatars/U11216002.png', import.meta.url),
+    github: 'kevinhuang09',
+  },
+  {
+    name: '孫振倫',
+    description: '',
+    avatar: new URL('/src/assets/avatars/U11216003.png', import.meta.url),
+    github: 'lun525',
+  },
+  {
+    name: '余珮璇',
+    description: '',
+    avatar: new URL('/src/assets/avatars/U11216004.png', import.meta.url),
+    github: 'yu-peihsuan',
+  },
+  {
+    name: '張恬熏',
+    description: '',
+    avatar: new URL('/src/assets/avatars/U11216011.png', import.meta.url),
+    github: 'Claraa1110',
+  },
+  {
+    name: '劉宸昕',
+    description: '',
+    avatar: new URL('/src/assets/avatars/U11216019.png', import.meta.url),
+    github: 'yuek666',
+  },
+  {
+    name: '張哲維',
+    description: '',
+    avatar: new URL('/src/assets/avatars/U11216021.png', import.meta.url),
+    github: 'cjw931027',
+  },
+  {
+    name: '楊淯丞',
+    description: '',
+    avatar: new URL('/src/assets/avatars/U11216047.png', import.meta.url),
+    github: 'yangyc1126',
+  },
+]
