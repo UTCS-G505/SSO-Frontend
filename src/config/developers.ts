591bdd717dedacb4c754415bdae168c60db0f8d7
AllenChang <111629702+BlueBoy247@users.noreply.github.com>
2025-11-03 03:51:00 +0800

    feat: update developers list

--- src/config/developers.ts
+++ src/config/developers.ts
@@ -122,9 +122,9 @@ export const developerConfig: DeveloperConfig[] = [
   },
   {
     name: '許建程',
-    description: '',
-    //avatar: new URL('/src/assets/avatars/U11316035.png', import.meta.url),
-    //github: 'jimkro',
+    description: 'I\'m looking for some motivation.',
+    avatar: new URL('/src/assets/avatars/U11316035.png', import.meta.url),
+    github: 'jimkro',
     jobs: [locker_borrow({ frontend: true })],
   },
   {
